9b9403e9513fc16b38c25f6abf4cc83c34b62bbc
Eadword <matt@xram.co>
2017-09-16 23:56:34 -0700

    Added custom U256

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
-extern crate u256;
-extern crate crypto;
+extern crate bincode;
 extern crate bytes;
+extern crate crypto;
+extern crate serde_json;
+extern crate serde;
+
+#[macro_use]
+extern crate serde_derive;
 
 
 #[cfg(test)]
-mod test;
 mod block;
+mod test;
+mod u256;
+
 
 pub fn do_stuff(x: i32) -> i32 {
     x * 5
