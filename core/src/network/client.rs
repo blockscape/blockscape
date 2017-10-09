0d1d40e3a5cc2eb7b647eb475469f9689c19d027
Daniel Beal <git@danb.email>
2017-10-08 19:04:04 -0700

    Add a bunch of client threading code

--- core/src/network/client.rs
+++ core/src/network/client.rs
@@ -2,7 +2,10 @@ use std::collections::{HashMap, VecDeque};
 use std::net::UdpSocket;
 use std::io::Error;
 use std::net::SocketAddr;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, RwLock, Mutex};
+use std::thread;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::Relaxed;
 
 use std::time::Duration;
 
@@ -23,7 +26,6 @@ use network::node::*;
 
 use signer::generate_private_key;
 use hash::hash_pub_key;
-
 use time::Time;
 
 #[derive(Serialize, Deserialize)]
@@ -79,39 +81,43 @@ impl<'a> NetworkContext<'a> {
     }
 }
 
-pub struct Client<'a> {
+pub struct Client {
 
     /// Configuration options for the behavior of the network client
     config: ClientConfig,
 
     /// Independant "connections" to each of the other NodeEndpoints we interact with
-    sessions: HashMap<SocketAddr, Session>,
-
-    recv_buf: HashMap<SocketAddr, Vec<u8>>,
+    sessions: RwLock<HashMap<SocketAddr, Session>>,
 
     /// The node object which represents my own system
     my_node: Arc<Node>,
 
     node_repo: RwLock<HashMap<U256, NodeRepository>>,
 
-    db: &'a mut Database,
+    db: Arc<Database>,
 
     /// List of all the networks we should be seeking node connections with
     connected_networks: Vec<U256>,
     /// The socket used to accept and invoke UDP communication
     socket: Option<UdpSocket>,
 
+    /// A mutex for sending on the socket--this only applies for sending, since we must always be reading
+    socket_mux: Mutex<()>,
+
     last_peer_seek: Time,
 
-    node_idx: usize
+    node_idx: usize,
+
+    /// Whether or not we are entered the exit state for the network interface
+    done: AtomicBool
 }
 
-impl<'a> Client<'a> {
+impl Client {
 
     /// The maximum amount of data that can be in a single message object (the object itself can still be in split into pieces at the datagram level)
     const MAX_PACKET_SIZE: usize = 1024 * 128;
 
-    pub fn new(db: &'a mut Database, config: ClientConfig) -> Client<'a> {
+    pub fn new(db: Arc<Database>, config: ClientConfig) -> Client {
         
         Client {
             db: db,
@@ -124,11 +130,12 @@ impl<'a> Client<'a> {
             config: config,
             node_repo: RwLock::new(HashMap::new()),
             connected_networks: Vec::new(),
-            sessions: HashMap::new(),
+            sessions: RwLock::new(HashMap::new()),
             socket: None,
+            socket_mux: Mutex::new(()),
             last_peer_seek: Time::from(0),
             node_idx: 0,
-            recv_buf: HashMap::new()
+            done: AtomicBool::new(false)
         }
 
         // Build my node object
@@ -139,8 +146,8 @@ impl<'a> Client<'a> {
             Ok(s) => {
 
                 // socket should read indefinitely
-                // this error will crash the program, but it should not fail in normal cases.
-                s.set_read_timeout(None).expect("Could not set socket read timeout");
+                // but we need to break every now and then to check thread signals
+                s.set_read_timeout(Some(Duration::from_millis(100))).expect("Could not set socket read timeout");
 
                 self.socket = Some(s);
 
@@ -150,7 +157,7 @@ impl<'a> Client<'a> {
         }
     }
 
-    fn node_scan(&mut self, network_id: &U256) {
+    fn node_scan(&self, network_id: &U256) {
         let mut removed_nodes = Vec::new();
 
         if !self.node_repo.read().unwrap().contains_key(network_id) {
@@ -160,7 +167,7 @@ impl<'a> Client<'a> {
         {
             let l = self.node_repo.read().unwrap();
             let node_repo = &l.get(network_id).unwrap();
-            if self.sessions.len() < self.config.min_nodes as usize {
+            if self.sessions.read().unwrap().len() < self.config.min_nodes as usize {
                 // try to connect a couple more nodes
                 'node_search: for _ in 0..3 {
 
@@ -174,13 +181,13 @@ impl<'a> Client<'a> {
                         self.node_idx = self.node_idx + 1;
 
                         let mut n = 0;
-                        while self.sessions.contains_key(&addr) {
+                        while self.sessions.read().unwrap().contains_key(&addr) {
                             peer = node_repo.get_nodes(self.node_idx);
                             self.node_idx = self.node_idx + 1;
                             n = n + 1;
 
                             // we have to prevent infinite looping here due to if all the nodes in the DB are conneted to (which is rare)
-                            if n > self.sessions.len() {
+                            if n > self.sessions.read().unwrap().len() {
                                 // we must be conneted to all nodes
                                 // this should basically only happen if the network itself is so fragmented and too small
                                 warn!("Node Repository lacks additional nodes to connect to; node shortage detected.");
@@ -190,7 +197,7 @@ impl<'a> Client<'a> {
 
                         // ready to connect
                         let sess = Session::new(self.my_node.clone(), peer, addr, network_id.clone());
-                        self.sessions.insert(addr, sess);
+                        self.sessions.write().unwrap().insert(addr, sess);
                     }
                     else {
                         // We have bogus data
@@ -212,10 +219,12 @@ impl<'a> Client<'a> {
     }
 
     /// Intended to be run in a single thread. Will receive packets, and react to them if necessary.
-    fn recv_loop(&mut self) {
+    fn recv_loop(&self) {
         // Let the sessions work on received packets
         // One kilobyte buffer should handle many packets, but some research is needed
         let mut buf: [u8; 1024] = [0; 1024];
+        let mut recv_buf: HashMap<SocketAddr, Vec<u8>> = HashMap::new();
+
         loop {
             let mut cont = true;
 
@@ -225,7 +234,6 @@ impl<'a> Client<'a> {
                 Ok((n, addr)) => {
 
                     let mut remove = false;
-                    let mut recv_buf = &mut self.recv_buf;
 
                     let recv_len = recv_buf.len();
 
@@ -284,59 +292,76 @@ impl<'a> Client<'a> {
             if let Some((p, addr)) = received_packet {
                 self.process_packet(&p.payload, &addr);
             }
+
+            if self.done.load(Relaxed) {
+                // stop receiving packets
+                break;
+            }
         };
     }
 
     /// Evaluate a single packet and route it to a session as necessary
     fn process_packet(&mut self, p: &Packet, addr: &SocketAddr) {
-        let mut context = NetworkContext::new(&self.node_repo, &self.db);
-        let mut inserted: Option<(SocketAddr, Node, U256)> = None;
-        
-        match self.sessions.get_mut(&addr) {
-            Some(sess) => {
-                sess.recv(&p, &mut context);
-            },
-            None => {
-                // session needs to be created
-                // special case! we will handle the introduce packet here
-                // if it is not an introduce packet, then this ends here.
-                // TODO: Make sure that the p.from is not bogus before we start trusting it
-                if let Message::Introduce { ref node, ref network_id } = p.msg {
-                    inserted = Some((addr.clone(), node.clone(), network_id.clone()));
+        {
+            let mut context = NetworkContext::new(&self.node_repo, &self.db);
+            let mut inserted: Option<(SocketAddr, Node, U256)> = None;
+            
+            match self.sessions.write().unwrap().get_mut(&addr) {
+                Some(sess) => {
+                    sess.recv(&p, &mut context);
+                },
+                None => {
+                    // session needs to be created
+                    // special case! we will handle the introduce packet here
+                    // if it is not an introduce packet, then this ends here.
+                    // TODO: Make sure that the p.from is not bogus before we start trusting it
+                    if let Message::Introduce { ref node, ref network_id } = p.msg {
+                        inserted = Some((addr.clone(), node.clone(), network_id.clone()));
+                    }
                 }
             }
-        }
 
-        if let Some((addr, node, network_id)) = inserted {
-            if let Some(repo) = self.node_repo.write().unwrap().get_mut(&network_id) {
-                // Here we must check that p.from is who we think it is, otherwise session could be hijacked
-                repo.new_node(node.clone());
+            if let Some((addr, node, network_id)) = inserted {
+                if let Some(repo) = self.node_repo.write().unwrap().get_mut(&network_id) {
+                    // Here we must check that p.from is who we think it is, otherwise session could be hijacked
+                    repo.new_node(node.clone());
 
-                // now we can create a new session
-                let mut sess = Session::new(self.my_node.clone(), repo.get(&hash_pub_key(&node.key)).unwrap(), addr, network_id);
-                sess.recv(&p, &mut context);
+                    // now we can create a new session
+                    let mut sess = Session::new(self.my_node.clone(), repo.get(&hash_pub_key(&node.key)).unwrap(), addr, network_id);
+                    sess.recv(&p, &mut context);
 
-                self.sessions.insert(addr, sess);
+                    self.sessions.write().unwrap().insert(addr, sess);
+                }
+                // otherwise drop the connection request
             }
-            // otherwise drop the connection request
-        }
 
-        // process data in the context: do we have anything to import?
-        // NOTE: Order is important here! We want the data to be imported in order or else it is much harder to construct
-        while let Some(txn) = context.import_txns.pop_front() {
-            self.report_txn(txn);
-        }
-        
-        while let Some(block) = context.import_blocks.pop_front() {
-            self.report_block(block);
+            // process data in the context: do we have anything to import?
+            // NOTE: Order is important here! We want the data to be imported in order or else it is much harder to construct
+            while let Some(txn) = context.import_txns.pop_front() {
+                self.report_txn(txn);
+            }
+            
+            while let Some(block) = context.import_blocks.pop_front() {
+                self.report_block(block);
+            }
         }
 
         // send any packets pending on the connection
         // session should always be valid at this point.
-        let s = &mut self.socket.as_ref().unwrap();
-        let sess = self.sessions.get_mut(&addr).unwrap();
 
         // TODO: Consider rate limiting, which might work well here
+        self.send_packets(&addr);
+    }
+
+    /// Send all the packets queued for the given session
+    pub fn send_packets(&mut self, addr: &SocketAddr) -> usize {
+        let s = &self.socket.as_ref().unwrap();
+        let mut count: usize = 0;
+
+        let lock = self.socket_mux.lock();
+
+        let sess = self.sessions.write().unwrap().get_mut(addr).unwrap();
+
         while let Some(p) = sess.pop_send_queue() {
 
             let mut rawp = RawPacket {
@@ -347,17 +372,17 @@ impl<'a> Client<'a> {
             let raw = serialize(&rawp, Bounded(Client::MAX_PACKET_SIZE as u64)).unwrap();
             s.send_to(&serialize(&raw.len(), Bounded(4)).unwrap()[..], sess.get_remote_addr());
             s.send_to(&raw[..], sess.get_remote_addr());
+
+            count += 1;
         }
+
+        count
     }
 
-    pub fn run(&mut self) {
-        // connection management
-        /*if self.last_peer_seek.millis() < Time::current().millis() - 3 * 1000 {
-            self.node_scan();
-            self.last_peer_seek = Time::current();
-        }*/
-
-        //let packet_vec = self.read_packets();
+    /// Spawns the threads and puts the networking into a full working state
+    pub fn run(this: Arc<Client>) {
+        // TODO: Do something about thread references! We need to be able to join to shut down the network thread
+        thread::Builder::new().name("Network Handler".into()).spawn(move || this.recv_loop());
     }
 
     pub fn report_txn(&self, txn: Txn) {
