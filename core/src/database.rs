192b1f8917848515d477c23cb95bb2e368bbd270
Eadword <matt@xram.co>
2017-10-09 15:30:20 -0700

    Added game data serilization to database

--- core/src/database.rs
+++ core/src/database.rs
@@ -3,17 +3,54 @@ use env;
 use mutation::{Change, Mutation};
 use rocksdb::{DB, WriteBatch};
 use rocksdb::Error as DBError;
+use serde::Serialize;
+use serde::de::DeserializeOwned;
 use std::collections::LinkedList;
 use std::fmt::Debug;
 use std::sync::RwLock;
 use txn::Txn;
+use bincode;
 
 /// Generic definition of a rule regarding whether changes to the database are valid.
 /// Debug implementations should state what the rule means/requires.
-trait MutationRule: Debug + Send + Sync {
+pub trait MutationRule: Debug + Send + Sync {
     /// Return Ok if it is valid, or an error explaining what rule was broken.
     fn is_valid(&self, database: &DB, mutation: &Mutation) -> Result<(), String>;
 }
+
+/// Storable objects are able to be stored in a `Database` instance.
+/// Example implementation:
+/// ```
+/// #[derive(Serialize, Deserialize)]
+/// struct Example {
+///     a: u8,
+///     b: u8
+/// }
+///
+/// impl Storable for Example {
+///     fn global_id() -> &'static [u8] { b"p" }
+///     fn instance_id(&self) -> Vec<u8> { vec![self.a, self.b] }
+/// }
+/// ```
+pub trait Storable: Serialize + DeserializeOwned {
+    /// Return a unique ID for the type, an example of this is b"plot", though the smallest
+    /// reasonable values would be better, e.g. b"p" for plot. All storable types must return
+    /// different IDs or there may be collisions.
+    fn global_id() -> &'static [u8];
+
+    /// Calculate and return a unique ID for the instance of this storable value. In the case of a
+    /// plot, it would simply be the plot ID. Must be
+    fn instance_id(&self) -> Vec<u8>;
+
+    /// Calculate and return the key-value of this object based on its global and instance IDs.
+    fn key(&self) -> Vec<u8> {
+        let mut key = Vec::new();
+        key.extend_from_slice(Self::global_id());
+        key.append(&mut self.instance_id());
+        key
+    }
+}
+
 
 /// A list of mutation rules
 pub type MutationRules = LinkedList<Box<MutationRule>>;
@@ -94,8 +131,7 @@ impl Database {
         for change in &mutation.changes {
             let key = {
                 let mut k = change.key.clone();
-                k.extend_from_slice(NETWORK_POSTFIX);
-                k
+                k.extend_from_slice(NETWORK_POSTFIX); k
             };
             
             let prior_value = db_lock.get(&key) // Result<Option<DBVector>, DBError>
@@ -124,8 +160,7 @@ impl Database {
         for change in &mutation.changes {
             let key = {
                 let mut k = change.key.clone();
-                k.extend_from_slice(NETWORK_POSTFIX);
-                k
+                k.extend_from_slice(NETWORK_POSTFIX); k
             };
             batch.put(&key, &change.value).expect("Failure when adding to rocksdb batch.");
         }
@@ -133,9 +168,17 @@ impl Database {
         (*db_lock).write(batch).map_err(|e| e.to_string())
     }
 
-    // pub fn get_game_data(&self) -> Vec<u8> {
+    pub fn put<S: Storable>(&mut self, obj: &S) -> Result<(), String> {
+        let key = {
+            let mut k = obj.key();
+            k.extend_from_slice(GAME_POSTFIX); k
+        };
 
-    // }
+        let value = bincode::serialize(obj, bincode::Infinite).expect("Error serializing game data");
+        
+        let db_lock = self.db.write().unwrap();
+        (*db_lock).put(&key, &value).map_err(|e| e.to_string())
+    }
 
     // pub fn get_block(hash: U256) -> Option<Block> {
 
