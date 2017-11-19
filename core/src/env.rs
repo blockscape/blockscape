f16a88f8ede283abad6cfbc588daa6ad957c1efe
Daniel Beal <git@danb.email>
2017-11-18 22:38:02 -0800

    Implement dedicated keyfile storage

--- core/src/env.rs
+++ core/src/env.rs
@@ -1,6 +1,9 @@
 use std::fs;
+use std::io::{Write, Read};
 use std::path::{Path,PathBuf};
 use std::env::home_dir;
+
+use openssl::pkey::PKey;
 
 /// Create the storage directory structure if it does not exist, and make sure it is valid if it
 /// does.
@@ -44,6 +47,49 @@ fn ensure_mkdir(p: &Path) {
             Err(why) => panic!("Could not create storage directory! Please check {} for access and try again. {:?}", p.display(), why.kind())
         }
     }
+}
+
+fn gen_key_path(name: &str) -> Box<Path> {
+    let mut pb = get_storage_dir().unwrap();
+    
+    pb.push("keys");
+    pb.push(format!("{}.pem", name));
+    
+    pb.into_boxed_path()
+}
+
+/// TODO: Needs proper error handling!
+pub fn save_key(name: &str, key: &PKey) -> bool {
+    let p = gen_key_path(name);
+
+    // should basically always succeed
+    if let Ok(pem) = key.private_key_to_pem() {
+        if let Ok(mut f) = fs::File::create(&p) {
+            if f.write_all(&pem).is_ok() {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+// TODO: Again, proper error handling needed
+pub fn load_key(name: &str) -> Option<PKey> {
+    let p = gen_key_path(name);
+
+    if let Ok(mut f) = fs::File::open(&p) {
+        let mut buf: Vec<u8> = Vec::new();
+
+        if f.read_to_end(&mut buf).is_ok() {
+            // parse pem
+            if let Ok(key) = PKey::private_key_from_pem(&buf) {
+                return Some(key);
+            }
+        }
+    }
+
+    None
 }
 
 // NOTE: Build currently fails on windows until we tell it how to save
