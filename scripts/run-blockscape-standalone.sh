966da22d90bea66ea94cb02fc38cfd6edd4f96bd
Daniel Beal <git@danb.email>
2018-01-24 16:04:18 -0800

    Fix ctrl-c on container to work

--- scripts/run-blockscape-standalone.sh
+++ scripts/run-blockscape-standalone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -gt 0 ]; then
-	docker run -t --rm -e RUST_LOG=blockscape=debug,blockscape_core=debug,info --net host dcr.buyme360.com/plu-capstone/blockscape:latest /blockscape "$@"
+	docker run -it --rm -e RUST_LOG=blockscape=debug,blockscape_core=debug,info --net host dcr.buyme360.com/plu-capstone/blockscape:latest /blockscape "$@"
 else
-	docker run -t --rm -e RUST_LOG=blockscape=debug,blockscape_core=debug,info --net host dcr.buyme360.com/plu-capstone/blockscape:latest
+	docker run -it --rm -e RUST_LOG=blockscape=debug,blockscape_core=debug,info --net host dcr.buyme360.com/plu-capstone/blockscape:latest
 fi
