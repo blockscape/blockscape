bc9d304c6e691ed2a382f3473079c021015de872
Daniel Beal <git@danb.email>
2018-01-26 19:15:59 -0800

    Add env variable which may or may not be needed

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -5,7 +5,7 @@ DIR=$(readlink -e $(dirname $0)/..)
 if docker ps -a | grep blockscape_build; then
     docker start -i blockscape_build
 else
-    docker run -i --name blockscape_build --sig-proxy=true -v $HOME/.cargo:/root/.cargo -v $DIR:/src dcr.buyme360.com/plu-capstone/rust:latest cargo build --manifest-path=/src/Cargo.toml -j8
+    docker run -i --name blockscape_build --sig-proxy=true -e LIBCLANG_PATH=/usr/lib/llvm-3.8/lib -v $HOME/.cargo:/root/.cargo -v $DIR:/src dcr.buyme360.com/plu-capstone/rust:latest cargo build --manifest-path=/src/Cargo.toml -j8
 fi
 
 docker build -t dcr.buyme360.com/plu-capstone/blockscape:latest ..
