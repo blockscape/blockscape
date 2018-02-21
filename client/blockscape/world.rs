bd120b5d694aefc3e5c06d76d28832c2c3e29fbb
Matthew Conover <matt@xram.co>
2018-02-21 14:34:37 -0800

    Preliminary work on dumb world-gen (#73)

--- client/blockscape/world.rs
+++ client/blockscape/world.rs
@@ -5,9 +5,31 @@ use super::*;
 
 const PLOT_SIZE: usize = 256;
 
-struct Plot<'a> {
+pub struct Plot<'a> {
     height_map: BitVec,
     structures: HashMap<Coord, Box<Structure>>,
     agents: HashMap<Coord, Box<Agent<'a>>>,
     mobs: HashMap<Coord, Box<Mobile<'a>>>
+}
+
+impl<'a> Plot<'a> {
+    pub fn generate() -> Plot<'a> {
+        Plot {
+            height_map: BitVec::from_elem(PLOT_SIZE * PLOT_SIZE, false),
+            // structures: hashmap!{ coord_to_index(Coord(PLOT_SIZE/2, PLOT_SIZE/2)) =>  }, //TODO: once we have a CPU structure definition, put it in the center of the plot.
+            structures: hashmap!(),
+            agents: HashMap::new(),
+            mobs: HashMap::new()
+        }
+    }
+
+    fn coord_to_index(coord: Coord) -> usize {
+        let x = if coord.0 >= 0 { coord.0 } else { PLOT_SIZE as i32 + coord.0 };
+        let y = if coord.1 >= 0 { coord.1 } else { PLOT_SIZE as i32 + coord.1 };
+
+        assert!(0 <= x && 0 <= y);
+        assert!(x < PLOT_SIZE as i32 && y < PLOT_SIZE as i32);
+
+        (PLOT_SIZE - y as usize) * PLOT_SIZE + x as usize
+    }
 }
